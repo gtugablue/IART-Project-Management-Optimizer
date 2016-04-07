b730c3ab5ed27775aa722a6e0317c519ad9773b8
Gustavo Silva <gtugablue@gmail.com>
2016-04-07 15:53:57 +0100

    Element done.

--- Project Management Optimizer/src/domain/Element.java
+++ Project Management Optimizer/src/domain/Element.java
@@ -1,5 +1,37 @@
 package domain;
 
-public class Element {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Element {
+	private String name;
+	private Map<Skill, Integer> skills;
+	
+	public Element(String name) {
+		this.name = name;
+		this.skills = new HashMap<Skill, Integer>();
+	}
+	
+	public Element(String name, Map<Skill, Integer> skills) {
+		this.name = name;
+		this.skills = skills;
+	}
+	
+	public void addSkill(Skill skill, int performance) {
+		this.skills.put(skill, performance);
+	}
+	
+	public int getSkillPerfomance(Skill skill) {
+		Integer performance = this.skills.get(skill);
+		if (performance == null) return -1;
+		return performance;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
 }
