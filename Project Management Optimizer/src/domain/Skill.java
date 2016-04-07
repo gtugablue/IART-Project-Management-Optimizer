3ed90d7c3cb352173e886410be4570c132963b5e
Gustavo Silva <gtugablue@gmail.com>
2016-04-07 15:55:33 +0100

    Skill done.

--- Project Management Optimizer/src/domain/Skill.java
+++ Project Management Optimizer/src/domain/Skill.java
@@ -1,5 +1,16 @@
 package domain;
 
 public class Skill {
-
+	private String name;
+	public Skill(String name) {
+		this.name = name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
 }
