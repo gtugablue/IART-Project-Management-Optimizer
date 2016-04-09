bb0a152d3e65fc17fb43bfd3743e06653f67ecfb
FilipaRamos <fimamora@hotmail.com>
2016-04-09 20:23:41 +0100

    Crossover and mutation

--- Project Management Optimizer/src/solver/genetic_algorithm/Algorithm.java
+++ Project Management Optimizer/src/solver/genetic_algorithm/Algorithm.java
@@ -1,44 +1,87 @@
 package solver.genetic_algorithm;
 
+import java.util.ArrayList;
+
 public class Algorithm {
 
 	private double mutationRate;
 	private boolean elitism;
-	
-	public Algorithm(double mutationRate, boolean elitism){
+
+	public Algorithm(double mutationRate, boolean elitism) {
 		this.mutationRate = mutationRate;
 		this.elitism = elitism;
 	}
-	
-	public double getMutationRate(){
+
+	public double getMutationRate() {
 		return mutationRate;
 	}
-	
-	public boolean getElitism(){
+
+	public boolean getElitism() {
 		return elitism;
 	}
-	
+
 	/**
-	 * Create a new population with 
+	 * Create a new population with
+	 * 
 	 * @param oldPopulation
 	 * @return Population
 	 */
-	private Population evolution(Population oldPopulation){
-		
+	private Population evolution(Population oldPopulation) {
+
 		Chromosome fittest;
-		
-		if(elitism)
+
+		if (elitism)
 			fittest = oldPopulation.getFittest();
 		else
 			fittest = oldPopulation.getRandom();
-			
+
 		Population newPopulation = new Population();
-		
+
 		newPopulation.populate(fittest);
-		
+
 		return newPopulation;
-			
+
 	}
-	
-	
+
+	/**
+	 * Crosover of two chromosomes
+	 * 
+	 * @param firstCh
+	 * @param secondCh
+	 * @param initialBit
+	 * @param finalBit
+	 * @return ArrayList<Chromosome>
+	 */
+	private ArrayList<Chromosome> crossover(Chromosome firstCh, Chromosome secondCh, int initialBit, int finalBit) {
+
+		ArrayList<Chromosome> chromosomes = new ArrayList<Chromosome>();
+
+		for (int i = initialBit; i < finalBit; i++) {
+			firstCh.getGenes()[i] = secondCh.getGenes()[i];
+			secondCh.getGenes()[i] = firstCh.getGenes()[i];
+		}
+
+		chromosomes.add(firstCh);
+		chromosomes.add(secondCh);
+
+		return chromosomes;
+
+	}
+
+	/**
+	 * Mutate chromosome in the desired distance
+	 * 
+	 * @param initialCh
+	 * @param initialBit
+	 * @param finalBit
+	 * @return Chromosome
+	 */
+	private Chromosome mutation(Chromosome initialCh, int mutationBit) {
+
+		initialCh.flipGene(mutationBit + 1);
+
+		return initialCh;
+
+	}
+
 }
--- Project Management Optimizer/src/solver/genetic_algorithm/Chromosome.java
+++ Project Management Optimizer/src/solver/genetic_algorithm/Chromosome.java
@@ -7,32 +7,36 @@ import optimizer.Problem;
 public class Chromosome {
 	private boolean[] genes;
 	private int fitness;
-	
+
 	public Chromosome(int length) {
 		this.genes = new boolean[length];
 		randomizeGenes();
 	}
-	
+
 	public Chromosome(boolean[] genes) {
 		this.genes = genes;
 	}
-	
+
+	public boolean[] getGenes() {
+		return genes;
+	}
+
 	public void flipGene(int index) {
 		this.genes[index] = !this.genes[index];
 	}
-	
+
 	public int evaluate(Problem problem) {
 		// TODO
 		return 0;
 	}
-	
+
 	private void randomizeGenes() {
 		Random r = new Random();
 		for (int i = 0; i < genes.length; i++) {
 			genes[i] = r.nextBoolean();
 		}
 	}
-	
+
 	public int getFitness() {
 		return this.fitness;
 	}
