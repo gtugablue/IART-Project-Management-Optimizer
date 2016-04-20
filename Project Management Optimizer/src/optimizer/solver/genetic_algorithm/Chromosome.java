0608be1bd03ccc04fdd475dcf28d65d86c179ae4
Gustavo Silva <gtugablue@gmail.com>
2016-04-20 21:24:43 +0100

    ...

--- Project Management Optimizer/src/optimizer/solver/genetic_algorithm/Chromosome.java
+++ Project Management Optimizer/src/optimizer/solver/genetic_algorithm/Chromosome.java
@@ -4,16 +4,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 import java.util.Random;
-import java.util.Set;
-import java.util.Stack;
 import java.util.Map.Entry;
 
 import optimizer.Problem;
