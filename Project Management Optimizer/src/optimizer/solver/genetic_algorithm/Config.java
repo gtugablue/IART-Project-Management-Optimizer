8119597322b5833597d50e129af959f535afc901
Gustavo Silva <silva95gustavo@gmail.com>
2016-05-27 22:26:30 +0100

    Forgot to add file in last commit.

---
+++ Project Management Optimizer/src/optimizer/solver/genetic_algorithm/Config.java
@@ -0,0 +1,8 @@
+package optimizer.solver.genetic_algorithm;
+
+public class Config {
+	public int populationSize;
+	public double mutationRate;
+	public double crossoverRate;
+	public int elitism;
+}
